1e55c97fe4d4131bb8d65a0376954b799a701b59
Muhammad Hadi <hadimuhammad@gmail.com>
2026-04-24 00:03:18 +0500

    feat: Supports Accel+Deriv Plots now

--- loadmatfile.py
+++ loadmatfile.py
@@ -1,48 +1,31 @@
-from lib.mat_loader import (
-    load_single,
-    load_range,
-    load_all,
-    load_latest
-)
+from lib.mat_loader import load_latest
 
 # ================= CONFIG =================
 
-MODE = "latest"  
-# "single"
-# "range"
-# "all"
-# "latest"
-
-#DATA_TYPE = "FSR"  
-DATA_TYPE = "IMU"  
-# "FSR" or "IMU"
-
-INDEX = 1
-RANGE_START = 1
-RANGE_END = 5
+DATA_TYPE = "IMU"
+# OPTIONS:
+# "IMU"
+# "FSR"
+# "IMU_ACCEL"
+# "IMU_DERIV"
 
 # ================= PREFIX =================
 
 if DATA_TYPE == "FSR":
     prefix = "FSR_data"
+
 elif DATA_TYPE == "IMU":
     prefix = "IMU_data"
+
+elif DATA_TYPE == "IMU_ACCEL":
+    prefix = "IMU_accel"
+
+elif DATA_TYPE == "IMU_DERIV":
+    prefix = "IMU_derivatives"
+
 else:
     raise ValueError("Invalid DATA_TYPE")
 
 # ================= EXECUTION =================
 
-if MODE == "single":
-    load_single(prefix, INDEX)
-
-elif MODE == "range":
-    load_range(prefix, RANGE_START, RANGE_END)
-
-elif MODE == "all":
-    load_all(prefix)
-
-elif MODE == "latest":
-    load_latest(prefix)
-
-else:
-    print("[ERROR] Invalid MODE")
+load_latest(prefix)
